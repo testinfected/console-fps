3b271606ab63242bc21714db598954a71e81f268
testinfected <vtence@gmail.com>
2022-01-02 14:28:24 -0500

    Make the player turn around using left and right keys
    Listen for keystrokes and make the player turn left or right by adjusting player point of view.
    
    To get proper animation, we need to account for elapsed time between frames. Since we only
    get key inputs and not key pressed/released events from the terminal, we also need to limit the
    frame rate to match the terminal key input delay.
    
    Finally, we display stats at the top of the screen (showing FPS and player info)
    when the SPACE key is pressed.

--- src/main/kotlin/Engine.kt
+++ src/main/kotlin/Engine.kt
@@ -1,3 +1,7 @@
+import java.util.concurrent.TimeUnit.SECONDS
+import kotlin.math.PI
+import kotlin.math.max
+
 class World(private val map: CharArray) {
     val width = 16
     val height = 16
@@ -12,11 +16,42 @@ class World(private val map: CharArray) {
     }
 }
 
+class AnimationTimer(private val frameDuration: Long) {
+    private val start: Long = nanos()
+    private var nextPulse: Long = start
+    private var lastPulse: Long = start
+
+    private val frameDelay: Long
+        get() {
+            val now = nanos()
+            val timeUntilPulse = (nextPulse - now) / 1_000_000
+            return max(0, timeUntilPulse)
+        }
+
+    fun pulse(): Long {
+        Thread.sleep(frameDelay)
+        val now = nanos()
+        val elapsedTime = now - lastPulse
+        recordPulse(now)
+        return elapsedTime
+    }
+
+    private fun recordPulse(now: Long) {
+        nextPulse = now + frameDuration
+        lastPulse = now
+    }
+
+    private fun nanos() = System.nanoTime()
+}
+
 
 class Camera(val fov: Angle)
 
 
-class Player(private val pos: Vector2D, val pov: Angle = 0.0) {
+class Player(private val pos: Vector2D, var pov: Angle = 0.0) {
     val x by pos::x
     val y by pos::y
+
+    // Player makes a full revolution in 3s
+    val rotationSpeed = 2.0 * PI / 3.0 / SECONDS.toNanos(1)
 }
--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -1,4 +1,6 @@
 import java.nio.charset.StandardCharsets
+import java.util.concurrent.TimeUnit.MILLISECONDS
+import java.util.concurrent.TimeUnit.SECONDS
 import kotlin.math.cos
 import kotlin.math.sin
 import kotlin.system.exitProcess
@@ -26,27 +28,52 @@ val map = """
 
 private val terminal = Terminal.connect(StandardCharsets.UTF_8)
 
+
 object Game {
     private val screen = terminal.screen
     private val keyboard = terminal.keyboard
     private val world = World.load(map)
+
+    // Set frame duration to account for terminal key repeat delay in order to get a smooth animation
+    private val timer = AnimationTimer(frameDuration = MILLISECONDS.toNanos(15))
+    // Camera has field of view of 90º (which is rather small)
     private val camera = Camera(fov = Math.PI / 4)
+    // Player starts at the middle of the map
     private val player = Player(v(8.0, 8.0))
 
     private val keystrokes = mutableListOf<Key>()
+    private var enableStats = false
 
     fun launch() {
         init()
 
         while (true) {
+            val elapsedTime = timer.pulse()
             processInput()
-            if (Key.Q in keystrokes) quit()
-            updateFrame()
+            updateFrame(elapsedTime)
+            displayStats(elapsedTime)
+            screen.renderFrame()
+        }
+    }
+
+    private fun displayStats(elapsedTime: Long) {
+        if (!enableStats) return
+        val info = "X = %3.2f Y = %3.2f, POV = %3.2f, FPS = %4.2f".format(
+            player.x,
+            player.y,
+            player.pov,
+            SECONDS.toNanos(1).toDouble() / elapsedTime
+        )
+        info.toCharArray().forEachIndexed { i, char ->
+            screen[i, 0] = char
         }
     }
 
     private fun processInput() {
+        keystrokes.clear()
         keystrokes += keyboard.readKeys()
+        if (Key.Q in keystrokes) quit()
+        if (Key.SPACE in keystrokes) enableStats = !enableStats
     }
 
     private fun quit() {
@@ -58,7 +85,14 @@ object Game {
         terminal.activateSingleCharacterMode()
     }
 
-    private fun updateFrame() {
+    private fun updateFrame(elapsedTime: Long) {
+        if (Key.LEFT in keystrokes) {
+            player.pov -= player.rotationSpeed * elapsedTime
+        }
+        if (Key.RIGHT in keystrokes) {
+            player.pov += player.rotationSpeed * elapsedTime
+        }
+
         (0 until screen.width).forEach { x ->
             // Simple ray casting to find distance to wall
             val rayAngle = player.pov - camera.fov / 2 + camera.fov * x / screen.width
@@ -100,12 +134,6 @@ object Game {
                 }
             }
         }
-
-        keystrokes.map { it.symbol }.flatMap { it.toList() }.forEachIndexed { i, char ->
-            screen[i % screen.width, 0] = char
-        }
-
-        screen.renderFrame()
     }
 }
 
